b79bef84ff456d9d96e09c928433cdb5bede9e6c
RafaelTOniz <161151458+RafaelTOniz@users.noreply.github.com>
2024-03-03 11:35:22 -0300

    exercicios Java

---
+++ IdiomaDoSistemaOperacional/src/main/java/com/mycompany/idiomadosistemaoperacional/IdiomaDoSistemaOperacional.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.idiomadosistemaoperacional;
+
+import java.util.Locale;
+
+/**
+ *
+ * @author casa
+ */
+public class IdiomaDoSistemaOperacional {
+
+    public static void main(String[] args) {
+        Locale loc = Locale.getDefault();
+        System.out.print("Seu sistema esta em ");
+        System.out.println(loc.getDisplayLanguage());
+    
+        
+    }
+}
