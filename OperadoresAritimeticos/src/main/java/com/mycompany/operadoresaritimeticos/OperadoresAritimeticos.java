16ae37fc2e43f81afa75784fca722002de027fc3
RafaelTOniz <161151458+RafaelTOniz@users.noreply.github.com>
2024-03-05 17:02:57 -0300

    exercícios em JAVA

---
+++ OperadoresAritimeticos/src/main/java/com/mycompany/operadoresaritimeticos/OperadoresAritimeticos.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.operadoresaritimeticos;
+
+/**
+ *
+ * @author casa
+ */
+public class OperadoresAritimeticos {
+
+    public static void main(String[] args) {
+        /*int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A Média é igual a " + m);*/
+        
+        /*int numero = 5;
+        int a = 5 + ++numero;
+        int b = 6 + numero++;
+        System.out.println(a);
+        System.out.println(b);
+        int numer = 5;
+        int c = 5 - --numer;
+        int d = 5 - numer--;
+        System.out.println(c);
+        System.out.println(d);*/
+
+        /*int x = 4;
+        x -= 2;
+        System.out.println(x);
+        */
+        
+        /*float v = 8.4f;
+        int ar = (int) Math.floor(v);
+        System.out.println(ar);
+        int a = (int) Math.ceil(v);
+        System.out.println(a);
+        int b = (int) Math.round(v);
+        System.out.println(b);
+        */
+        
+        double ale = Math.random();
+        int n = (int)(5 + ale *(10-5));
+        System.out.println(n);
+        
+        double al = Math.random();
+        int m = (int)(15 + al *(50-15));
+        System.out.println(m);
+    }
+}
